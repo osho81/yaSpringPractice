5fda512d73bb79e8adc5f170e8ea7740244aca3a
osho81 <ossi.hour@gmail.com>
2022-09-21 23:24:49 +0200

    Add exception class; resource not found

--- src/main/java/com/osho/restBasics/exception/ResourceNotFoundException.java
+++ src/main/java/com/osho/restBasics/exception/ResourceNotFoundException.java
@@ -1,4 +1,46 @@
 package com.osho.restBasics.exception;
 
-public class ResourceNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+// (Optional) Exception class for throwing customized exceptions
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L; // Version control of objects
+
+    private String name;
+    private String field;
+    private Object value;
+
+    public ResourceNotFoundException(String message, String name, String field, Object value) {
+        super(String.format("%s not found with %s : '%S'", name, field, value));
+        this.name = name;
+        this.field = field;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
